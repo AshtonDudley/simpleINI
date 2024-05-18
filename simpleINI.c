265246981e3a06120cdb0b2c856302f14d7511bc
Ashton Dudley <ashtondudley03@gmail.com>
2024-05-17 21:57:40 -0300

    Updated string func

--- simpleINI.c
+++ simpleINI.c
@@ -15,6 +15,24 @@ typedef struct {
 } token_data_t;
 
 
+char* stripWhitespace(char* str) {
+    char* end;
+
+    // Trim leading space
+    while (isspace((unsigned char)*str)) str++;
+
+    if (*str == 0)  // All spaces?
+        return str;
+
+    // Trim trailing space
+    end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end)) end--;
+
+    // Write new null terminator character
+    end[1] = '\0';
+    return str;
+}
+
 
 // Extracts a string between two quotations marks.
 // Returns NULL if no string is found.
@@ -33,13 +51,49 @@ const char* extractStringToken(const char* token) {
 
 // Returns a string matched to a given key.
 // Returns NULL if no string is found.
-const char* readString(FILE* fptr, const char* key) {
+const char* readString(FILE* fptr, char* key) {
+    char fileInput[MAX_BUFFER_SIZE];
 
+    while (fgets(fileInput, MAX_BUFFER_SIZE, fptr)) {
+
+
+        if (validLine(fileInput))
+        {
+            // fprintf(stderr, "%s", fileInput); // Print the entire file
+
+            char* token;
+            char* next_token = NULL;
+            char delimiter[] = "=";
+            char* currentLine = _strdup(fileInput);
+
+            if (currentLine == NULL) {
+                fprintf(stderr, "DEBUG: Memory allocation failed\n");
+                return NULL;
+            }
+
+            // Split the string into tokens 
+            token = strtok_s(currentLine, delimiter, &next_token);
+            // token[strcspn(token, "\n")] = '\0';
+            
+             if (strcmp(stripWhitespace(currentLine), key) == 0) {
+                char* subString = extractStringToken(next_token);
+                if (next_token){
+                    return subString; 
+                }
+                return NULL;
+            }
+
+
+
+        }
+    }
+    return NULL;
 }
 
+// Reads and pritns the entire contents of an .ini file
 int readAll(FILE* fptr) {
     char fileInput[MAX_BUFFER_SIZE];
-      
+
     while (fgets(fileInput, MAX_BUFFER_SIZE, fptr)) {
         
         
@@ -111,17 +165,57 @@ int validLine(char* line) {
 }
 
 
+void stringsTest() {
+    // Test 1
+    FILE* fptr = fopen("config.ini", "r");
+    if (fptr == NULL) {
+        perror("Failed to open .ini file");
+        return EXIT_FAILURE;
+    }
+
+    char* sn1 = readString(fptr, "serialNumber");
+    fprintf(stdout, "GOOD  : %s\n", sn1);
+    fclose(fptr);
+
+    // Test 2
+    fptr = fopen("config.ini", "r");
+    if (fptr == NULL) {
+        perror("Failed to open .ini file");
+        return EXIT_FAILURE;
+    }
+
+    char* sn2 = readString(fptr, "serialNumber ");
+    fprintf(stdout, "SERIAL: %s\n", sn2);
+    fclose(fptr);
+
+    // Test 3
+    fptr = fopen("config.ini", "r");
+    if (fptr == NULL) {
+        perror("Failed to open .ini file");
+        return EXIT_FAILURE;
+    }
+
+    char* sn3 = readString(fptr, "serialNumberrr");
+    fprintf(stdout, "SERIAL: %s\n", sn3);
+    fclose(fptr);
+
+    // Test 4
+    fptr = fopen("config.ini", "r");
+    if (fptr == NULL) {
+        perror("Failed to open .ini file");
+        return EXIT_FAILURE;
+    }
+
+    char* sn4 = readString(fptr, "serialNumbe");
+    fprintf(stdout, "SERIAL: %s\n", sn4);
+    fclose(fptr);
+    return;
+}
+
 int main()
 {
     printf("Hello, World!\n");
-    FILE* fptr = fopen("config.ini", "r");
-
-    if (fptr == NULL) {
-        perror("Failed to open file");
-        return EXIT_FAILURE;
-    }
-    readAll(fptr);
-    fclose(fptr);
+    stringsTest();   
 
     return 0;
 }
